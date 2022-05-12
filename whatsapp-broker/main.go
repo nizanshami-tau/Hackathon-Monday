8b4bb91a034a12b1413fdf8c399f23cc02b5fa2f
guysv <sviryguy@gmail.com>
2022-05-12 22:36:43 +0300

    6

--- whatsapp-broker/main.go
+++ whatsapp-broker/main.go
@@ -188,6 +188,7 @@ func (s *WhatsappService) OAuthCallback(w http.ResponseWriter, req *http.Request
 	form.Add("client_id", CLIENT_ID)
 	form.Add("client_secret", CLIENT_SECRET)
 	form.Add("code", code)
+	oauthLog.Infof("CATCHME 2 %+v", form)
 	resp, err := http.PostForm("https://auth.monday.com/oauth2/token", form)
 	if err != nil {
 		oauthLog.Errorf("Monday auth returned error: %v", err)
