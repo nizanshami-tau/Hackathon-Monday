95954c5a0e377d8457ab7e8757783f21e82996b7
guysv <sviryguy@gmail.com>
2022-05-12 19:47:37 +0300

    6

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/nizanshami-tau/Hackathon-Monday
 
 go 1.17
+
+require (
+	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	go.mau.fi/libsignal v0.0.0-20220425070825-c40c839ee6a0 // indirect
+	go.mau.fi/whatsmeow v0.0.0-20220511113019-fd17667ead01 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ whatsapp-broker/main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	_ "github.com/mattn/go-sqlite3"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	waLog "go.mau.fi/whatsmeow/util/log"
+	"net/http"
+	"os/exec"
+	"strings"
+	"sync"
+	"time"
+)
+
+// const DB_PATH string = "/var/local/whatsapp-broker/app.db"
+const DB_PATH string = "/tmp/myapp.db"
+
+type WhatsappService struct {
+	container *sqlstore.Container
+	logger    waLog.Logger
+	idToQr    sync.Map
+}
+
+func NewWhatsappService() (*WhatsappService, error) {
+	dbLog := waLog.Stdout("Database", "DEBUG", true)
+	container, err := sqlstore.New("sqlite3", fmt.Sprintf("file:%s?_foreign_keys=on", DB_PATH), dbLog)
+	if err != nil {
+		return nil, err
+	}
+
+	return &WhatsappService{
+		container: container,
+		logger:    waLog.Stdout("Service", "DEBUG", true),
+	}, nil
+}
+
+func (s *WhatsappService) SendWhatsappQR(w http.ResponseWriter, req *http.Request) {
+	deviceStore := s.container.NewDevice()
+
+	clientLog := waLog.Stdout("Client", "DEBUG", true)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+
+	qrCtx, _ := context.WithTimeout(context.Background(), 60*time.Second)
+	qrChan, _ := client.GetQRChannel(qrCtx)
+	err := client.Connect()
+	if err != nil {
+		clientLog.Errorf("Connect error: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	clientLog.Debugf("Waiting on qr channel")
+	evt := <-qrChan
+	clientLog.Debugf("QR channel called with event: %+v", evt)
+	if evt.Event == "code" {
+		imgBuf := &bytes.Buffer{}
+		cmd := exec.CommandContext(req.Context(), "bash", "-c", "qrencode -t png -o - | base64")
+		cmd.Stdin = bufio.NewReader(strings.NewReader(evt.Code))
+		cmd.Stdout = imgBuf
+
+		err := cmd.Run()
+		if err != nil {
+
+			clientLog.Errorf("Error rendering qr: '%s', %v, %v", evt.Code, err)
+			w.WriteHeader(500)
+			return
+		}
+
+		id, err := uuid.NewRandom()
+		if err != nil {
+			clientLog.Errorf("Error generating id: '%s', %v, %v", evt.Code, err)
+			w.WriteHeader(500)
+			return
+		}
+
+		go func() {
+			<-qrCtx.Done()
+			s.idToQr.Delete(id)
+		}()
+
+		if _, loaded := s.idToQr.LoadOrStore(id, qrChan); loaded {
+			clientLog.Errorf("Login session id %s already exists in mapping", id.String())
+			w.WriteHeader(500)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf(`<!DOCTYPE html>
+<html>
+	<head>
+		<title>Whatsapp QR login</title>
+	</head>
+	<body>
+		<img src="data:image/png;base64, %s", alt="Login QR Code"/>
+	</body>
+	<script>
+		async function subscribe() {
+			let response = await fetch("/qr-callback?id=%s");
+			if (response.status != 200) {
+				console.log(response.statusText);
+			} else {
+				window.location.replace("/foobar?id=%s");
+			}
+		}
+		subscribe();
+	</script>
+</html>`,
+			imgBuf.String(), id.String(), id.String())))
+
+		return
+	} else {
+		clientLog.Errorf("Login event: %s", evt.Event)
+		w.WriteHeader(500)
+		return
+	}
+}
+
+func (s *WhatsappService) QrCallback(w http.ResponseWriter, req *http.Request) {
+	callbackLog := waLog.Stdout("Client", "DEBUG", true)
+
+	idStr := req.URL.Query().Get("id")
+	if idStr == "" {
+		callbackLog.Errorf("Login session id does not exist in query string")
+		w.WriteHeader(500)
+		return
+	}
+
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		callbackLog.Errorf("Failed to parse session id: %v, %s", err, idStr)
+		w.WriteHeader(500)
+		return
+
+	}
+	qrChan, ok := s.idToQr.Load(id)
+	if !ok {
+		callbackLog.Errorf("Login session id %s does not exist in mapping", idStr)
+		w.WriteHeader(500)
+		return
+	}
+
+	evt := <-qrChan.(<-chan whatsmeow.QRChannelItem)
+	callbackLog.Debugf("evt: %+v", evt)
+	w.WriteHeader(200)
+	return
+}
+
+func main() {
+	s, err := NewWhatsappService()
+	if err != nil {
+		panic(err)
+	}
+
+	public := http.NewServeMux()
+	public.HandleFunc("/whatsapp-qr", s.SendWhatsappQR)
+	public.HandleFunc("/qr-callback", s.QrCallback)
+
+	http.ListenAndServe(":3000", public)
+}
